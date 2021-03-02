0231d992cb888c47aa3a9b2c3717d075da55a3a7
Mike Ona <ona_mike@yahoo.com>
2021-03-01 17:18:04 -0800

    Updated script

--- script.js
+++ script.js
@@ -1,51 +1,84 @@
-function operate(a,b,operator){
-    let result = 0;
-    if(operator == "+"){
-        result = Math.add(a,b);
-    }
-    else if(operator == "-"){
-        result = Math.subtract(a,b);
-    }
-    else if(operator == "*"){
-        result = Math.multiply(a,b);
-    }
-    else if(operator == "/"){
-        result = Math.divide(a,b);
-    }
-    return result;
-}
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+//const buttons = document.querySelectorAll('button');
 
-
-
-
-
-
-
-function clearDisplay(){
-    document.getElementById("display").innerHTML = "";
-}
-
-
-
-
-var previousNumber = "";
 
 var elements = document.getElementsByClassName("button");
+var previousDigit = "";
+var currentValue = "";
+var evaluateFlag = false;
+var operandFlag = false;
+
 
 function displayValue(){
-    var attribute = this.getAttribute("value");
-    if(attribute=="/" || attribute=="*" || attribute=="-" || attribute=="+"){
-        var value = "";
-        previousNumber = "";
+    var buttonValue = this.getAttribute("value");
+
+    if(buttonValue == "/" || buttonValue == "*" || buttonValue == "-" || buttonValue == "+"){
+
+        if(evaluateFlag){
+            currentValue = operate(Number(firstOperand),Number(secondOperand),buttonValue);
+            firstOperand = currentValue; //iffy about this
+            operandFlag = true;
+        }
+
+        document.getElementById("display").innerHTML = currentValue;
+        currentValue = "";
+        previousDigit = "";
+        evaluateFlag = true;
+
     }
     else{
-        var value=previousNumber+attribute;
+
+
+        currentValue = previousDigit + buttonValue;
+        document.getElementById("display").innerHTML = currentValue;
+        previousDigit = currentValue;
+
+        if(evaluateFlag){
+            secondOperand = currentValue;
+        }
+        else{
+            firstOperand = currentValue;
+            console.log(firstOperand);
+        }
+        
+
     }
 
-    document.getElementById("display").innerHTML = value;
-    previousNumber = value;
+    
 };
 
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', displayValue, false);
+}
+
+
+function clearDisplay(){
+    document.getElementById("display").innerHTML = "";
+    previousDigit = "";
+    currentValue = "";
+    evaluateFlag = false;
+}
+
+function operate(a,b,operator){
+    if(operator == "+"){
+        return a+b;
+    }
+    else if(operator == "-"){
+        return a-b;
+    }
+    else if(operator == "*"){
+        return a*b;
+    }
+    else if(operator == "/"){
+        if(y==0){
+            return "yikes";
+        }
+        else
+            return a/b;  
+    }
+ 
 }
